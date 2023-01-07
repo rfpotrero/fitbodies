736fd4f5c27dbf0ad9cb457bc30986b9c1321f95
rfpotrero <raulf.potrero@gmail.com>
2023-01-07 17:06:19 +0000

    feat: add templates dirs to settings.py
    Adding templates and allauth to templates in seetings.py #4

--- fitbodies/settings.py
+++ fitbodies/settings.py
@@ -64,7 +64,8 @@ ROOT_URLCONF = 'fitbodies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
