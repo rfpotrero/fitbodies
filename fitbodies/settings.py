88372fa75b9def76c83358dea520cf33c09e77f1
rfpotrero <raulf.potrero@gmail.com>
2023-01-11 16:10:58 +0000

    fix: correct allowed host in settings file

--- fitbodies/settings.py
+++ fitbodies/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://fitbodies.herokuapp.com/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fitbodies.herokuapp.com"]
 
 
 # Application definition
