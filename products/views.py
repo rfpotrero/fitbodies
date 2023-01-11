eb306f9a64d982820e89b1e98ebb69f0c9c84aca
rfpotrero <raulf.potrero@gmail.com>
2023-01-11 09:35:47 +0000

    feat: update views and urls for add_product
    updating views and URL to handle add new add product functionality #54

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -34,3 +35,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
