bf093b236d26f22049c8487cba76c6dce24bcbe2
rfpotrero <raulf.potrero@gmail.com>
2023-01-11 19:41:08 +0000

    fix: correct typo in AWS variable media storage

--- fitbodies/settings.py
+++ fitbodies/settings.py
@@ -172,7 +172,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fitbodies'
-    AWS_S3_REGION_NAME = 's3.eu-west-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-1.amazonaws.com'
