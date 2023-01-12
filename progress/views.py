9b9f0e86c1664b9bbcc96ff087356b599be56eb8
rfpotrero <raulf.potrero@gmail.com>
2023-01-12 16:53:02 +0000

    feat: craete progress view to view/update progress
    Create view to update the progress. The view tries to find a progress
    in the registered user and if there is no none will display and empty
    form for the user to update their progress. Pending to fix new users
    with no progress #61

--- progress/views.py
+++ progress/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
 from .models import UserProgress
 
+from .forms import UserProgressForm
+
+
 def view_progress(request):
 
-    template = 'progress/progress.html'
+
+    if request.method == 'POST':
+        form = UserProgressForm(request.POST)
+        if form.is_valid():
+        # check if user already has a UserProgress object
+            try:
+                user_progress = UserProgress.objects.get(user=request.user)
+                # update the existing object with form data
+                user_progress.height = form.cleaned_data['height']
+                user_progress.weight = form.cleaned_data['weight']
+                user_progress.weight_goal = form.cleaned_data['weight_goal']
+                user_progress.chest = form.cleaned_data['chest']
+                user_progress.waist = form.cleaned_data['waist']
+                user_progress.save() # Update the exisitng info.
+            except UserProgress.DoesNotExist:
+                # create a new UserProgress object if none exists
+                user_progress = UserProgress(user=request.user,
+                height=form.cleaned_data['height'],
+                weight=form.cleaned_data['weight'],
+                weight_goal=form.cleaned_data['weight_goal'],
+                chest=form.cleaned_data['chest'],
+                waist=form.cleaned_data['waist'])
+                user_progress.save()
+
+            template = 'progress/progress.html'
+            context = {
+                'form': form,
+            }
+
+            return render(request, template, context)
+        
+    # if request method is GET display the form
+    form = UserProgressForm()
     context = {
-      
+        'form': form,
+        'user_progress':user_progress
     }
+    template = 'progress/progress.html'
     return render(request, template, context)
+
