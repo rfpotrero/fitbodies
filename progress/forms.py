11e188ed65da625f468fb75d952b15917ba48f15
rfpotrero <raulf.potrero@gmail.com>
2023-01-12 16:50:57 +0000

    feat: create forms.py forms.py
    Create forms.py for progress app #64

--- progress/forms.py
+++ progress/forms.py
@@ -1,3 +1,29 @@
 from django import forms
 from .models import UserProgress
 
+
+class UserProgressForm(forms.ModelForm):
+    class Meta:
+        model = UserProgress
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'height': 'Height',
+            'weight': 'Weight',
+            'waist': 'measurements',
+            'chest': 'measurements',
+            'weight_goal': 'Goal Weight',
+        }
+
+        self.fields['height'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
