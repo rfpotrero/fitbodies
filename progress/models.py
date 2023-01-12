7694503b4036e8c5086401fcaf3163f20f099fef
rfpotrero <raulf.potrero@gmail.com>
2023-01-12 16:26:52 +0000

    feat: create models for progress app
    Creating models for progress app. #63

--- progress/models.py
+++ progress/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProgress(models.Model):
@@ -12,3 +14,4 @@ class UserProgress(models.Model):
 
     def __st__(self):
         return self.user.first_name
+        
