45c00e64653b92d5c7ed323e4a798e2583d471ec
rfpotrero <raulf.potrero@gmail.com>
2023-01-14 10:20:42 +0000

    feat: complete view to add review and urls
    Completed view to add review and supporting urls. #71

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_review, name='add_review'),
+    path('add_review/<product_id>/', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Review
+from .forms import ReviewForm
+from products.models import Product
 
-def add_review(request):
+
+def add_review(request, product_id):
+    """
+    View to add review to product.
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.product = product
+            comment.save()
+
+            return redirect('product_detail', product_id=product.pk)
+
+    else:
+        form = ReviewForm()
 
     template = 'reviews/add_review.html'
     context = {
-
+        'form': form,
+        'product': product
     }
     return render(request, template, context)
