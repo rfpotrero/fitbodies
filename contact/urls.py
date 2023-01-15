96250f5a025735344d5805512c3696b2e076eae3
rfpotrero <raulf.potrero@gmail.com>
2023-01-15 13:07:31 +0000

    feat: add urls.py to contact #80

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views., name=''),
+    path('add_contact/', views.add_contact, name='contact'),
+    path('contact_success/', views.contact_success, name='contact_success'),
 ]
