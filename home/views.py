b5d8e29f9374ccdb77d681cb8de4b5b98d022c52
rfpotrero <raulf.potrero@gmail.com>
2023-01-15 17:24:54 +0000

    style: change the following elements template #87
    Create a drop down menu for the shop. Include the following elements
    in the main-nav: contact us, Personal trainers and classes pages.

--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def personaltrainers(request):
+    """ A view to return the Personal Trainers page """
+
+    return render(request, 'home/index.html')
+
+
+def classes(request):
+    """ A view to return the clases """
+
+    return render(request, 'home/index.html')
