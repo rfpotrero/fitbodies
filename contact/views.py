5bd01be90146a3cb21af3969f222096bb5ed3dba
rfpotrero <raulf.potrero@gmail.com>
2023-01-15 13:02:02 +0000

    feat: add views to capture the contact form
    Adding views to capture contact and to display contact success page #84

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import ContactForm
+
+
+def add_contact(request):
+    """
+    View to process contacts 
+    """
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('contact_success')
+    else:
+
+        form = ContactForm()
+        template = 'contact/add_contact.html'
+        context = {
+            'form': form
+        }
+
+        return render(request, template, context)
+
+
+def contact_success(request):
+    """
+    View to generate a sucess page
+    """
+
+    template = 'contact/contact_success.html'
+    return render(request, template)
+
