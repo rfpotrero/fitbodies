1154c3023b1ee2110025e0f34d865c11d2a90839
rfpotrero <raulf.potrero@gmail.com>
2023-01-15 13:04:51 +0000

    feat: create contact form
    adding forms.py to render the contact form #83

--- contact/forms.py
+++ contact/forms.py
@@ -6,4 +6,10 @@ class ContactForm(forms.ModelForm):
     """
     Model to capture users contact
     """
-    
+
+    class Meta:
+        model = Contact
+        fields = ['name',
+                  'email',
+                  'message',
+                  'contact_reason']
