bb2350d326752b4b8d28edfec6fe3b0049069d79
rfpotrero <raulf.potrero@gmail.com>
2023-01-17 13:56:16 +0000

    feat: admin are allowed to see contact enquiry
    Adding contact equiry for admins to reviews. #86

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):  
+    list_display = (
+        'email',
+        'contact_reason',
+        'message',
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
