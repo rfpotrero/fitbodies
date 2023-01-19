6f836f305e44969f7c26baa6e8435b95d3aada78
rfpotrero <raulf.potrero@gmail.com>
2023-01-19 20:29:58 +0000

    refactor: refactor code to be PEP8 compliant
    refactor code to be PEP8 compliant

--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
-
-from checkout.models import Order
 
 
 @login_required
@@ -18,18 +17,19 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-    
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form': form, 
+        'form': form,
         'orders': orders,
         'on_profile_page': True
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
