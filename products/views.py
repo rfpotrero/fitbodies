00972ea5009db53c42861d44381056247c30cdfd
rfpotrero <raulf.potrero@gmail.com>
2023-01-22 21:41:35 +0000

    refactor: remove unused code for add product

--- products/views.py
+++ products/views.py
@@ -45,29 +45,6 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
-
-@login_required
-def add_product(request):
-    """ Add a product to the store """
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_detail', args=[product.id]))
-        else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
-    else:
-        form = ProductForm()
-
-    template = 'products/add_product.html'
-    context = {
-        'form': form,
-    }
-
-    return render(request, template, context)
-
-
 @login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
